8965b22c638932d8121b4e30583d7bcc557327d9
Jam Martins <aondongujam1993@gmail.com>
2022-10-30 18:42:28 +0100

    uploadGit saved

---
+++ image/Javascript practice/index1.js
@@ -0,0 +1,21 @@
+
+
+
+function areaOfTriangle(base, height){
+    let sum =  1/2 * base *height
+    console.log(sum);
+} 
+areaOfTriangle(2,5)
+
+function areaOfCircle(pi,r,r){
+    
+let formular = 3.142 * 3 * 3
+return formular;
+}
+
+ 
+console.log(areaOfCircle('3.142', '3'));
+
+const myName =name=>alert('Good day')
+
+myName('JamMartins');
