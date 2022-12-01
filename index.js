97b71a3aa49e83ce41c1f883e3e9467d2b25028a
Jam Martins <aondongujam1993@gmail.com>
2022-12-01 01:58:08 +0100

    new upload

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
-let action = prompt(' please input value for name')
+let alert = prompt(' please input value for name')
 if(name){
     console.log(true);
-}
-else{
-console.log(' ivalid value for name');
 }
